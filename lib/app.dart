a3096bfd1429e053ac5e4cc0bb49a7a8de58d8f9
Iskander <100300679+Iskander2323@users.noreply.github.com>
2026-03-04 16:33:53 +0500

    Provide Firestore repo and use router
    Inject FirestoreRepository into the App via a required constructor parameter and expose it through MultiRepositoryProvider. Replace the placeholder home Scaffold with MaterialApp.router using the app routes, and add necessary imports for flutter_bloc, repository, and routes. This wires up dependency injection and app routing.

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weight_tracker/domain/firestore_repository.dart';
+import 'package:weight_tracker/routes.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({super.key, required this.firestoreRepository});
+
+  final FirestoreRepository firestoreRepository;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello, Flutter!'),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(create:  (context) => firestoreRepository),
+      ],
+      child: MaterialApp.router(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
+        routerConfig: routes,
       ),
     );
   }
