d16efb190ba85de3222bdcc8ca740dfb845d3a3f
Iskander <100300679+Iskander2323@users.noreply.github.com>
2026-03-30 16:03:36 +0500

    Add AllRecords route and AppRouteNames enum
    Introduce AppRouteNames enum to centralize route names and import AllRecordsPage. Add a nested GoRoute for path 'all_records' under the home route that builds AllRecordsPage and use the enum for the route name. This organizes route naming and registers the All Records page in the router.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,13 @@
 import 'package:go_router/go_router.dart';
+import 'package:weight_tracker/ui/all_records_page/ui/all_records_page.dart';
 import 'package:weight_tracker/ui/home_page/home_page.dart';
+
+enum AppRouteNames {
+  Home,
+  AllRecords;
+
+  String get name => toString().split('.').last;
+}
 
 final routes = GoRouter(
   initialLocation: '/',
@@ -9,6 +17,13 @@ final routes = GoRouter(
       builder: (context, state) {
         return const HomePage();
       },
+      routes: [
+        GoRoute(
+          path: 'all_records',
+          name: AppRouteNames.AllRecords.name,
+          builder: (context, state) => AllRecordsPage(),
+        ),
+      ],
     ),
   ],
 );
