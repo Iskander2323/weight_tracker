e9013bdea267157fe6c44511245e433b94de7159
Iskander <100300679+Iskander2323@users.noreply.github.com>
2026-03-30 15:13:29 +0500

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,14 @@
 import 'package:go_router/go_router.dart';
-import 'package:weight_tracker/ui/home_page.dart';
+import 'package:weight_tracker/ui/home_page/home_page.dart';
 
-final routes = GoRouter(initialLocation: '/', routes: [
-  GoRoute(path: '/', builder: (context, state) {
-    return const HomePage();
-  }),
-]);
+final routes = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return const HomePage();
+      },
+    ),
+  ],
+);
